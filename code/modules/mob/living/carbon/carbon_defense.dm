HolyTigerGod/Voidcrew

Refactor can_inject, and introduce try_inject (#56816)
Splits can_inject into can_inject and try_inject. The latter can perform side effects, while the former cannot. Moved the show_error bool parameter to a flag that try_inject checks.

Changes the signature to can_inject(mob/user, target_zone, injection_flags). This was previously redefined in several places, making it impossible to use named parameters. This refactor now allows named parameters to be used, and thus avoiding the ugliness of TRUE, FALSE, FALSE in parameters.

code/modules/mob/living/carbon/carbon_defense.dm
@@ -173,7 +173,7 @@
 
 /mob/living/carbon/attack_paw(mob/living/carbon/human/M, modifiers)
 
-	if(can_inject(M, TRUE))
+	if(try_inject(M, injection_flags = INJECT_TRY_SHOW_ERROR_MESSAGE))
 		for(var/thing in diseases)
 			var/datum/disease/D = thing
 			if((D.spread_flags & DISEASE_SPREAD_CONTACT_SKIN) && prob(85))

code/modules/mob/living/carbon/human/human_defense.dm
@@ -249,7 +249,7 @@
 	if(M.limb_destroyer)
 		dismembering_strike(M, affecting.body_zone)
 
-	if(can_inject(M, 1, affecting))//Thick suits can stop monkey bites.
+	if(try_inject(M, affecting, injection_flags = INJECT_TRY_SHOW_ERROR_MESSAGE))//Thick suits can stop monkey bites.
 		if(..()) //successful monkey bite, this handles disease contraction.
 			var/damage = rand(M.dna.species.punchdamagelow, M.dna.species.punchdamagehigh)
 			if(!damage)

XeonMations/The-Final-Nights

code/modules/mob/living/carbon/carbon_defense.dm
@@ -184,8 +184,9 @@
 	return FALSE
 
 
-/mob/living/carbon/attack_paw(mob/living/carbon/human/user, list/modifiers)
-	if(can_inject(user, TRUE))
+/mob/living/carbon/attack_paw(mob/living/carbon/human/user, modifiers)
+
+	if(try_inject(user, injection_flags = INJECT_TRY_SHOW_ERROR_MESSAGE))
 		for(var/thing in diseases)
 			var/datum/disease/D = thing
 			if((D.spread_flags & DISEASE_SPREAD_CONTACT_SKIN) && prob(85))

code/modules/mob/living/carbon/human/human_defense.dm
@@ -257,7 +257,7 @@
 	if(user.limb_destroyer)
 		dismembering_strike(user, affecting.body_zone)
 
-	if(can_inject(user, 1, affecting))//Thick suits can stop monkey bites.
+	if(try_inject(user, affecting, injection_flags = INJECT_TRY_SHOW_ERROR_MESSAGE))//Thick suits can stop monkey bites.
 		if(..()) //successful monkey bite, this handles disease contraction.
 			var/damage = rand(user.dna.species.punchdamagelow, user.dna.species.punchdamagehigh)
 			if(!damage)