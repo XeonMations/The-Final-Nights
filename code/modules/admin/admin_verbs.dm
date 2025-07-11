HolyTigerGod/Voidcrew

[Ready for Review] Admin lua scripting (#65635)
Co-authored-by: Mothblocks <35135081+Mothblocks@users.noreply.github.com>

code/modules/admin/admin_verbs.dm
@@ -199,6 +199,7 @@ GLOBAL_PROTECT(admin_verbs_debug)
 	/client/proc/cmd_admin_toggle_fov,
 	/client/proc/cmd_admin_debug_traitor_objectives,
 	/client/proc/spawn_debug_full_crew,
+	/client/proc/open_lua_editor,
 	/client/proc/validate_puzzgrids,
 	/client/proc/debug_spell_requirements,
 	)

XeonMations/The-Final-Nights

code/modules/admin/admin_verbs.dm
@@ -208,7 +208,8 @@ GLOBAL_PROTECT(admin_verbs_debug)
 #endif
 	/client/proc/check_timer_sources,
 	/client/proc/toggle_cdn,
-	/client/proc/allow_browser_inspect
+	/client/proc/allow_browser_inspect,
+	/client/proc/open_lua_editor
 	)
 GLOBAL_LIST_INIT(admin_verbs_possess, list(GLOBAL_PROC_REF(possess), GLOBAL_PROC_REF(release)))
 GLOBAL_PROTECT(admin_verbs_possess)